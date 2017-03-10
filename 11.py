baef39bc5e94f1f60047d5e6c7b8d7c0e9722f05
nilses11 <nilses11@github.com>
2017-03-10 17:32:45 +0100

    tmpedit

--- 11.py
+++ 11.py
@@ -10,32 +10,49 @@ counter = 1
 
 #FUNCTIONS
 def status():
-	p = "r=" + str(r) + " t=" + str(t) + " i=" + str(i)
-	print p
-
+	return "r=" + str(r) + " t=" + str(t) + " i=" + str(i)
 
 def buy(amount):
 	global r, t
 	if amount <= r:
 		r-=amount
 		t+=amount
-		print "Confirmed t+="+str(amount)
+		return "Confirmed t+="+str(amount)
 
 def invest(amount):
 	global r, i
 	if amount <= r:
 		r-=amount
 		i+=amount
-		print "Confirmed i+="+str(amount)
+		return "Confirmed i+="+str(amount)
 
 def next():
 	global counter, r
 	counter+=1
-	p = "round " + str(counter)
-	print p
 	r+=i
+	return "round " + str(counter)
 
 
-p = "round " + str(counter)
+print "round " + str(counter)
 while loopvar != Q:
 	loopvar = input()
+	print str(loopvar)
+
+	
+	
+	""""
+	from random import randint
+
+#aiModes = ["AGR","DEF","BAL","SMA"]
+#aiMode = aiModes[randint(0,3)]
+
+#CHANGEABLES
+air = 1000
+ait = 0
+aii = 100
+
+def runAi():
+	#global aiMode # impl agr først
+	buy(1000)
+	attack(500)
+	"""
